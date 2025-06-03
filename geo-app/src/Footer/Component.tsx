8787f75296074371af12d0a84aa7d8c6af20e4a3
Adrian Voicu <adrian@myslut.tyb.net>
2025-06-03 13:11:09 +0300

    first commit

---
+++ geo-app/src/Footer/Component.tsx
@@ -0,0 +1,64 @@
+import { getCachedGlobal } from '@/utilities/getGlobals'
+import Link from 'next/link'
+import React from 'react'
+
+import type { Footer } from '@/payload-types'
+
+import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
+import { CMSLink } from '@/components/Link'
+import { Logo } from '@/components/Logo/Logo'
+
+export async function Footer() {
+  const footerData: Footer = await getCachedGlobal('footer', 1)()
+
+  const navItems = footerData?.navItems || []
+
+  return (
+    <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
+      <div className="flex items-center justify-center container p-8">
+        <Link className="flex items-center" href="/">
+          <Logo />
+        </Link>
+      </div>
+
+      <div className="container py-8 gap-6 flex flex-col md:flex-row md:justify-between">
+        {/* <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
+          <ThemeSelector />
+          <nav className="flex flex-col md:flex-row gap-4">
+            {navItems.map(({ link }, i) => {
+              return <CMSLink className="text-white" key={i} {...link} />
+            })}
+          </nav>
+        </div> */}
+
+        <div className="block">
+          <nav className="flex flex-col">
+            {navItems.slice(0, 3).map(({ link }, i) => {
+              return <CMSLink className="text-white" key={i} {...link} />
+            })}
+          </nav>
+        </div>
+
+        <div className="block">
+          <nav className="flex flex-col">
+            {navItems.slice(3, 6).map(({ link }, i) => {
+              return <CMSLink className="text-white" key={i} {...link} />
+            })}
+          </nav>
+        </div>
+        {/* <ThemeSelector /> */}
+      </div>
+      <hr className="mb-4"></hr>
+      <div className="block text-center text-xs/5 p-4 mb-4 inter">
+        <p>Copyright © 2025</p>
+        <p>Web App di proprietà di Nesti Società Cooperativa Impresa Sociale</p>
+        <p>Sede legale: Via Savasta, 16 - 95047 Paternò (CT)</p>
+        <p>PEC: nestimpresasociale@pec.it - P.IVA: 06057800879 - N. REA: CT - 462431</p>
+        <p>
+          Il sito internet è realizzato e interamente gestito da nesti società cooperativa impresa
+          sociale
+        </p>
+      </div>
+    </footer>
+  )
+}
