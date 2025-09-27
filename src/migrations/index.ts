fe351c4e90a5681e9ee44e0d6dd2db668d30055a
Adrian Voicu <adrian@myslut.tyb.net>
2025-09-27 16:20:07 +0300

    fixed map + other changes 13

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250927_131819_optional_name_here from './20250927_131819_optional_name_here';
+
+export const migrations = [
+  {
+    up: migration_20250927_131819_optional_name_here.up,
+    down: migration_20250927_131819_optional_name_here.down,
+    name: '20250927_131819_optional_name_here'
+  },
+];
